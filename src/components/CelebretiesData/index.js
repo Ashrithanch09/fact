77993544b00268ed89d8537f0678cbfb666f276d
Bishnu Mishra <bishnumishra01996@gmail.com>
2022-12-12 16:36:58 +0530

    assignment completed

--- src/components/CelebretiesData/index.js
+++ src/components/CelebretiesData/index.js
@@ -4,10 +4,16 @@ import { MdOutlineEdit } from "react-icons/md";
 import celebrities from "../../data/celebrities.json";
 import "./style.css";
 import Confirmation from "../Confirmation";
+
 const CelebretiesData = () => {
   const [celebritiesList, setCelebritiesList] = useState([]);
   const [isShow, setIsShow] = useState(null);
   const [confirm, setConfirm] = useState(false);
+  const [isEdit, setIsEdit] = useState(true);
+  const [editIndex, setEditIndex] = useState(null);
+  const [newdata, setNewdata] = useState(celebritiesList[editIndex]);
+  const [query, setQuery] = useState("");
+  const [buttonhide, setButtonhide] = useState(false);
 
   useEffect(() => {
     celebrities && setCelebritiesList(celebrities);
@@ -17,10 +23,20 @@ const CelebretiesData = () => {
     setIsShow((prev) => (prev === i ? null : i));
   };
 
-  const deleteCelebrity = () => {
+  const handleDelete = () => {
     setConfirm(true);
   };
 
+  const handleCancel = () => {
+    console.log("cancelled");
+    setConfirm(false);
+  };
+
+  const deleteCelebrity = (id, i) => {
+    console.log("delete data", id, i);
+    setCelebritiesList(celebritiesList.filter((delId) => delId.id !== id));
+    setConfirm(false);
+  };
   const ageCoverter = (date) => {
     let dob = new Date(date);
     let monthDeff = Date.now() - dob.getTime();
@@ -30,74 +46,245 @@ const CelebretiesData = () => {
     return age;
   };
 
+  const handlchange = (e) => {
+    if (e.target.name === "country") {
+      if (e.target.value === "number") {
+        console.log("true");
+      }
+    }
+    setNewdata({ ...newdata, [e.target.name]: e.target.value });
+    setButtonhide(true);
+  };
+
+  const handleEdit = (index) => {
+    setNewdata(celebritiesList[index]);
+    setEditIndex(index);
+    setIsEdit(false);
+  };
+
+  const handleSave = (e) => {
+    if (newdata.first.length === 0) {
+      alert("You can not keep the blank");
+      setIsEdit(false);
+    } else {
+      setIsEdit(true);
+      setButtonhide(false);
+    }
+    if (newdata.country.length === 0) {
+      alert("You can not keep the blank");
+      setIsEdit(false);
+    } else {
+      setIsEdit(true);
+    }
+
+    celebritiesList.splice(editIndex, 1, newdata);
+  };
+
+  const searchChangeHandle = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const checkType = (data) => {
+    if (isNaN(data)) {
+      return data;
+    } else {
+      return "";
+    }
+  };
   return (
     <div className="container">
-      {confirm && <Confirmation />}
-      {celebritiesList.map((data, i) => (
-        <div className="accordian" key={data.id}>
-          <div className="celebrity-detail">
-            <div className="celebrity">
-              <img
-                src={data.picture}
-                alt={data.picture}
-                className="celebrity-picture"
-              />
-              <input
-                type="text"
-                value={data.first + data.last}
-                readOnly={true}
-              />
-              {/* <p className="celebrity-name">
-                {data.first} {data.last}
-              </p> */}
-            </div>
-            <span className="plus-minus" onClick={() => openAccordian(i)}>
-              +
-            </span>
-          </div>
-          {i === isShow && (
-            <div>
-              <div className="celebrities-dob-gen-country">
-                <div>
-                  <p>Age</p>
-                  <input
-                    type="text"
-                    value={ageCoverter(data.dob)}
-                    readOnly={true}
-                  />
-                  {/* <p className="date-of-birth">{ageCoverter(data.dob)}</p> */}
-                </div>
-                <div>
-                  <p> Gender</p>
-                  <select name={data.gender}>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                    <option value="Transgender">Transgender</option>
-                    <option value="Rather Not Say">Rather Not Say</option>
-                  </select>
+      <div>
+        <input
+          type="text"
+          className="search"
+          placeholder="Search user"
+          onChange={searchChangeHandle}
+        />
 
-                  {/* <p className="gendar">{data.gender}</p> */}
+        {celebritiesList
+          .filter(
+            (user) =>
+              user.first.toLowerCase().includes(query) ||
+              user.last.toLowerCase().includes(query)
+          )
+          .map((data, i) => (
+            <div className="accordian" key={data.id}>
+              <div className="celebrity-detail">
+                <div className="celebrity">
+                  <img
+                    src={data.picture}
+                    alt={data.picture}
+                    className="celebrity-picture"
+                  />
+                  {isEdit ? (
+                    <p>{data.first + " " + data.last}</p>
+                  ) : editIndex !== i ? (
+                    <p>{data.first + " " + data.last}</p>
+                  ) : (
+                    <div className="InputNameFiled">
+                      <input
+                        name="first"
+                        type="text"
+                        className="edit_Name"
+                        value={newdata.first}
+                        onChange={handlchange}
+                      />
+                      <input
+                        name="last"
+                        type="text"
+                        className="edit_Name"
+                        value={newdata.last}
+                        onChange={handlchange}
+                      />
+                    </div>
+                  )}
                 </div>
                 <div>
-                  <p>Counter</p>
-                  <p className="country">{data.country}</p>
+                  {isShow !== i ? (
+                    <span
+                      className="plus-minus"
+                      onClick={() => openAccordian(i)}
+                    >
+                      +
+                    </span>
+                  ) : (
+                    <span
+                      className="plus-minus"
+                      onClick={() => openAccordian(i)}
+                    >
+                      -
+                    </span>
+                  )}
                 </div>
               </div>
-              <div className="description">
-                <p>Description</p>
-                <p>{data.description}</p>
-              </div>
-              <div className="icons">
-                <RiDeleteBin6Line
-                  className="delete"
-                  onClick={() => deleteCelebrity(data.id)}
-                />
-                <MdOutlineEdit className="edit" />
-              </div>
+              {i === isShow && (
+                <div>
+                  <div className="celebrities-dob-gen-country">
+                    <div>
+                      <p>Age</p>
+                      {isEdit ? (
+                        <p>{ageCoverter(data.dob)}</p>
+                      ) : editIndex !== i ? (
+                        <p>{ageCoverter(data.dob)}</p>
+                      ) : (
+                        <input
+                          name="dob"
+                          type="date"
+                          className="edit_age"
+                          value={newdata.dob || ageCoverter(data.dob)}
+                          onChange={handlchange}
+                        />
+                      )}
+                    </div>
+                    <div>
+                      <p> Gender</p>
+                      {isEdit ? (
+                        <p className="gendar">{data.gender}</p>
+                      ) : editIndex !== i ? (
+                        <p className="gendar">{data.gender}</p>
+                      ) : (
+                        <select
+                          name="gender"
+                          value={newdata.gender}
+                          className="edit_gendar"
+                          onChange={handlchange}
+                        >
+                          <option value="Male">Male</option>
+                          <option value="Female">Female</option>
+                          <option value="Transgender">Transgender</option>
+                          <option value="Rather Not Say">Rather Not Say</option>
+                        </select>
+                      )}
+                    </div>
+
+                    <div>
+                      <p>Counter</p>
+
+                      {isEdit ? (
+                        <p>{data.country}</p>
+                      ) : editIndex !== i ? (
+                        <p>{data.country}</p>
+                      ) : (
+                        <input
+                          type="text"
+                          name="country"
+                          className="edit_country"
+                          value={checkType(newdata.country)}
+                          onChange={handlchange}
+                        />
+                      )}
+                    </div>
+                  </div>
+                  <div className="description">
+                    <p>Description</p>
+                    {isEdit ? (
+                      <p>{data.description}</p>
+                    ) : editIndex !== i ? (
+                      <p>{data.description}</p>
+                    ) : (
+                      <textarea
+                        rows="9"
+                        cols="53"
+                        name="description"
+                        className="edit_description"
+                        value={newdata.description}
+                        onChange={handlchange}
+                      ></textarea>
+                    )}
+                  </div>
+                  {isEdit ? (
+                    <div>
+                      <div className="icons">
+                        <RiDeleteBin6Line
+                          className="delete"
+                          onClick={handleDelete}
+                        />
+                        <MdOutlineEdit
+                          className="edit"
+                          onClick={() => handleEdit(i)}
+                        />
+                      </div>
+                      <div>
+                        {confirm && (
+                          <Confirmation
+                            onCancled={handleCancel}
+                            onDelete={() => deleteCelebrity(data.id, i)}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  ) : editIndex !== i ? (
+                    <div>
+                      <div className="icons">
+                        <RiDeleteBin6Line
+                          className="delete"
+                          onClick={handleDelete}
+                        />
+                        <MdOutlineEdit
+                          className="edit"
+                          onClick={() => handleEdit(i)}
+                        />
+                      </div>
+                      <div>
+                        {confirm && (
+                          <Confirmation
+                            onCancled={handleCancel}
+                            onDelete={() => deleteCelebrity(data.id, i)}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  ) : (
+                    <div>
+                      <button onClick={() => setIsEdit(true)}>cancel</button>
+                      {buttonhide && <button onClick={handleSave}>save</button>}
+                    </div>
+                  )}
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      ))}
+          ))}
+      </div>
     </div>
   );
 };
--- src/components/Confirmation/index.js
+++ src/components/Confirmation/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./style.css";
 
-const Confirmation = () => {
+const Confirmation = (props) => {
   return (
     <div className="confirmation-box">
       <div className="conf-message">
         <p>Are you sure you want to delete?</p>
       </div>
       <div className="confirmation-button">
-        <button className="conf-cancel">Cancel</button>
-        <button className="conf-delete">Delete</button>
+        <button className="conf-cancel" onClick={props.onCancled}>
+          No
+        </button>
+        <button className="conf-delete" onClick={props.onDelete}>
+          Yes
+        </button>
       </div>
     </div>
   );
